d654e129cf9719eba0bd0325256d0cc11388a6c5
9FS <pray4spam@googlemail.com>
2024-05-09 22:35:24 +0000

    updated sleep

--- KFSsleep/KFSsleep.py
+++ KFSsleep/KFSsleep.py
@@ -12,33 +12,33 @@ def sleep_mod(sleep_modulus: int) -> None:
     - sleep_modulus: the modulus to use to determin when to stop sleeping
     """
 
-    now_DT: dt.datetime     # now
-    return_DT: dt.datetime  # when to return
+    now_dt: dt.datetime     # now
+    return_dt: dt.datetime  # when to return
 
-    now_DT=dt.datetime.now(dt.timezone.utc)
-    return_DT=now_DT-dt.timedelta(seconds=now_DT.timestamp()%sleep_modulus)+dt.timedelta(seconds=sleep_modulus)
+    now_dt=dt.datetime.now(dt.timezone.utc)
+    return_dt=now_dt-dt.timedelta(seconds=now_dt.timestamp()%sleep_modulus)+dt.timedelta(seconds=sleep_modulus)
 
 
-    sleep_until(return_DT)  # sleep until calculated return time
+    sleep_until(return_dt)  # sleep until calculated return time
 
     return
 
 
-def sleep_until(return_DT: dt.datetime) -> None:
+def sleep_until(return_dt: dt.datetime) -> None:
     """
     Blockingly sleeps until the specified datetime and then returns. Expects timezone-aware datetime object.
 
     Arguments:
-    - return_DT: the datetime when to return
+    - return_dt: the datetime when to return
     """
 
-    now_DT: dt.datetime             # now
+    now_dt: dt.datetime             # now
     time_until_return: dt.timedelta # how long still until return
     time_to_sleep: float            # how many seconds is next sleep? depends on time_until_return magnitude
 
 
-    while (now_DT:=dt.datetime.now(dt.timezone.utc))<return_DT:                             # as long as return datetime not reached yet: sleep
-        time_until_return=return_DT-now_DT                                                  # update time until return
+    while (now_dt:=dt.datetime.now(dt.timezone.utc))<return_dt:                             # as long as return datetime not reached yet: sleep
+        time_until_return=return_dt-now_dt                                                  # update time until return
         try:
             time_to_sleep=10**(math.floor(math.log10(time_until_return.total_seconds()))-1) # time to sleep is 1 magnitude smaller than time until return
         except ValueError:                                                                  # in case time until return is exactly 0: log10 crashes, give time to sleep 0
